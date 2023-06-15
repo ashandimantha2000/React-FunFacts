d832e7577908eac7a7c9c76bcf68b3e3293a5366
ashandimantha2000 <ashandimanthalk@gmail.com>
2023-06-15 15:45:51 +0530

    navigation

---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Nav() {
+    return (
+        <nav>
+            <img src="../src/assets/images/React-icon.png" className="nav-icon" />
+            <h3 className="nav-icon-text">ReactFacts</h3>
+            <h4 className="nav-text">React Course - Project 1</h4>
+        </nav>
+    );
+}
